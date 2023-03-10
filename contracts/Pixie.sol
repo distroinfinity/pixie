3dbcfd5ac8065456f5cec654610bdc5278876364
distroinfinity <manurajput2911@gmail.com>
2023-03-10 17:40:43 +0530

    user and file realted smart contracts changes

---
+++ contracts/Pixie.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./PixieEnums.sol";
+
+contract Pixie {
+    struct User {
+        string name;
+        bool exists;
+        uint256[] myFiles;
+    }
+    struct File {
+        uint256 id;
+        address owner;
+        string content;
+        Access access;
+    }
+
+    modifier onlyAuthor(uint256 _id) {
+        require(
+            msg.sender == idToFile[_id].owner,
+            "You are not the owner of this file"
+        );
+        _;
+    }
+    modifier userExists() {
+        require(!addressToUser[msg.sender].exists, "User Does not exist");
+        _;
+    }
+    address payable public owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private fileId;
+
+    mapping(uint256 => File) private idToFile;
+    mapping(address => User) private addressToUser;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+        console.log("Contract deployed, owner set");
+    }
+
+    // utility functions
+    function createFile() public userExists {
+        uint256 id = fileId.current();
+        fileId.increment();
+        File memory tempFile = File(id, msg.sender, "abc", Access.Private);
+        console.log("file created", id);
+        idToFile[id] = tempFile;
+        addressToUser[msg.sender].myFiles.push(id);
+    }
+
+    // getter setter
+    // get file
+    function getFile(uint256 _id) public view returns (File memory) {
+        return idToFile[_id];
+    }
+
+    //get user
+    function getUser(address _user) public view returns (User memory) {
+        return addressToUser[_user];
+    }
+
+    // get visibility of a file
+    function checkAccess(uint256 _id) public view returns (Access) {
+        return idToFile[_id].access;
+    }
+
+    // change access of a file
+    function changeAccess(uint256 _id, Access _access) public onlyAuthor(_id) {
+        idToFile[_id].access = _access;
+    }
+}
---
+++ contracts/PixieEnums.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+enum Access {
+    Private, // 0 - default value is the first element in the list
+    Public, // 1
+    Restricted // 2
+}
